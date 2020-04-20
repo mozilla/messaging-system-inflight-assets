8f532a769d8f636f89d889f2022d444f9249c87f
Andrei Oprea <andrei.br92@gmail.com>
2020-04-20 13:24:27 +0200

    Validate targeting for PRs

--- scripts/validate.py
+++ scripts/validate.py
@@ -6,7 +6,17 @@ import sys
 import jsonschema
 
 from jsonschema.exceptions import best_match, ValidationError
+from pyjexl.jexl import JEXL
 
+# Create a jexl evaluator
+EVALUATOR = JEXL()
+EVALUATOR.add_binary_operator('intersect', 40, lambda x, y: set(x).intersection(y))
+EVALUATOR.add_transform('date', lambda x: 0)
+EVALUATOR.add_transform('length', lambda x: 0)
+EVALUATOR.add_transform('preferenceValue', lambda x: False)
+EVALUATOR.add_transform('mapToProperty', lambda x, y: [])
+EVALUATOR.add_transform('keys', lambda x: [])
+EVALUATOR.add_transform('bucketSample', lambda x, y, z, q: False)
 
 USAGE = """
     Usage:
@@ -15,6 +25,18 @@ USAGE = """
     Exmaple:
         validate.py ./cfr.json ./schema/cfr.schema.json
 """
+
+
+def validate_item_targeting(item):
+    print("Validate", item['id'])
+    jexl_expression = item.get("targeting") or item.get("filter_expression")
+    try:
+        result = list(EVALUATOR.validate(jexl_expression))
+        if len(result) > 0:
+            raise Exception(result[0])
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 
 def validate(src_path, schema_path):
@@ -30,6 +52,7 @@ def validate(src_path, schema_path):
                 match = best_match([err])
                 print("Validation error: {}".format(match.message))
                 sys.exit(1)
+            validate_item_targeting(item)
 
     print("Passed!")
 
