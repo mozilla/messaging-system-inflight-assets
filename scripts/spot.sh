91b93485c23c33d674c6a6b73fc3bafed58a1346
Andrei Oprea <andrei.br92@gmail.com>
2020-05-11 18:54:58 +0200

    Add script to import from Experimenter

---
+++ scripts/spot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PYTHON=python3
+EXPERIMENTER_URL=https://experimenter.services.mozilla.com/api/v1/experiments
+RECIPE_URL=${EXPERIMENTER_URL}/$1/recipe/
+
+if [[ -z $1 ]]; then
+  curl -SsL ${EXPERIMENTER_URL}/ | jq -r '.[].slug'
+  echo "Choose from one of the available slugs: $0 <slug_name>"
+else
+  json_input=$(curl -SsL ${RECIPE_URL} | jq -r '.arguments')
+  ${PYTHON} -c 'import sys, yaml, json; yaml.dump(json.load(sys.stdin), sys.stdout)
+    '<<<${json_input} > $1.yaml
+fi
